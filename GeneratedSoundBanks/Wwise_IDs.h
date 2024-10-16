c51ea6eb9988b32709b8d4000f852b1206384358
Gabeman21 <130042062+Gabeman21@users.noreply.github.com>
2024-10-16 20:44:35 +0100

    Commit

---
+++ GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,231 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID FS_PLAY = 518478193U;
+        static const AkUniqueID PLAY_INTERACTIVE_UNREAL = 4184446064U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_STAC = 32095635U;
+        static const AkUniqueID PLAYING_AMBIENCE = 1919845324U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace AREA_STATE
+        {
+            static const AkUniqueID GROUP = 3979687176U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FARMLANDAREA1 = 1423669922U;
+                static const AkUniqueID FARMLANDAREA2 = 1423669921U;
+                static const AkUniqueID FARMLANDAREA3 = 1423669920U;
+                static const AkUniqueID FARMLANDAREA4 = 1423669927U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace AREA_STATE
+
+        namespace FACILITY_STACKOBOT
+        {
+            static const AkUniqueID GROUP = 33713465U;
+
+            namespace STATE
+            {
+                static const AkUniqueID COMBAT = 2764240573U;
+                static const AkUniqueID DEATH = 779278001U;
+                static const AkUniqueID INTRO = 1125500713U;
+                static const AkUniqueID JOURNEY = 3551468019U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID SPAWN = 71202558U;
+            } // namespace STATE
+        } // namespace FACILITY_STACKOBOT
+
+        namespace GAME_STATUS
+        {
+            static const AkUniqueID GROUP = 2453052416U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace GAME_STATUS
+
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace STATE
+            {
+                static const AkUniqueID AMBIENT = 77978275U;
+                static const AkUniqueID COMBAT = 2764240573U;
+                static const AkUniqueID DEATH = 779278001U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID VICTORY = 2716678721U;
+            } // namespace STATE
+        } // namespace MUSIC
+
+        namespace MUSIC_INTERACTION_UNREAL
+        {
+            static const AkUniqueID GROUP = 2608212327U;
+
+            namespace STATE
+            {
+                static const AkUniqueID AMBIENT = 77978275U;
+                static const AkUniqueID COMBAT = 2764240573U;
+                static const AkUniqueID DEATH = 779278001U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID VICTORY = 2716678721U;
+            } // namespace STATE
+        } // namespace MUSIC_INTERACTION_UNREAL
+
+        namespace ROOM_STATE
+        {
+            static const AkUniqueID GROUP = 2789492242U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INDOOR = 340398852U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OUTDOOR = 144697359U;
+            } // namespace STATE
+        } // namespace ROOM_STATE
+
+        namespace TUNNEL
+        {
+            static const AkUniqueID GROUP = 3059984139U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OFF = 930712164U;
+                static const AkUniqueID ON = 1651971902U;
+            } // namespace STATE
+        } // namespace TUNNEL
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace AMBBIOMTYPE
+        {
+            static const AkUniqueID GROUP = 2181999364U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FOREST = 491961918U;
+                static const AkUniqueID GRASSLAND = 1199171794U;
+                static const AkUniqueID MOUNTAIN = 513139656U;
+                static const AkUniqueID SWAMP = 2907906111U;
+            } // namespace SWITCH
+        } // namespace AMBBIOMTYPE
+
+        namespace FOOTSTEPMATERIAL_TYPE
+        {
+            static const AkUniqueID GROUP = 1420842295U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID ROCK = 2144363834U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPMATERIAL_TYPE
+
+        namespace GROUNDWETNESSSWITCHES
+        {
+            static const AkUniqueID GROUP = 4178538517U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DRY = 630539344U;
+                static const AkUniqueID DUMP = 2382162035U;
+                static const AkUniqueID SOAKED = 1905651656U;
+                static const AkUniqueID WET = 1181096339U;
+            } // namespace SWITCH
+        } // namespace GROUNDWETNESSSWITCHES
+
+        namespace LOCOMOTION_TYPE
+        {
+            static const AkUniqueID GROUP = 748991833U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID RUNNING = 3863236874U;
+                static const AkUniqueID SNEAK = 2884887403U;
+                static const AkUniqueID SPRINT = 1296465089U;
+                static const AkUniqueID WALKING = 340271938U;
+            } // namespace SWITCH
+        } // namespace LOCOMOTION_TYPE
+
+        namespace TIME_OF_THE_DAY
+        {
+            static const AkUniqueID GROUP = 2219227717U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID AFTERNOON = 390531879U;
+                static const AkUniqueID EVENING = 2905060079U;
+                static const AkUniqueID MIDDAY = 1211181101U;
+                static const AkUniqueID MORNING = 1924633667U;
+                static const AkUniqueID NIGHT = 1011622525U;
+            } // namespace SWITCH
+        } // namespace TIME_OF_THE_DAY
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DISTANCE_FROM_OBJECT = 4182290991U;
+        static const AkUniqueID FADE_OUT_REVERB = 1839481305U;
+        static const AkUniqueID MATERIAL_TYPE = 1097155889U;
+        static const AkUniqueID RTPC_PLAYERSPEED = 2653406601U;
+        static const AkUniqueID RTPC_RAINAMOUNT = 1294084109U;
+        static const AkUniqueID RTPC_TIMEOFTHEDAY = 1198362850U;
+        static const AkUniqueID STEP_BLENDER = 1982879522U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID MUSIC_STAC = 3260736504U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID BOXES = 2016903052U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID OBJECTS = 1695690031U;
+        static const AkUniqueID REVERB = 348963605U;
+        static const AkUniqueID VERB = 3657263530U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID ROOM_1 = 1941896084U;
+        static const AkUniqueID ROOM_2 = 1941896087U;
+        static const AkUniqueID TUNNEL_REVERB = 3400062578U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
